2b717b2eec30deb04e31fd6cc1eb630e2a254f1c
Maurice Schmitz <maurice@familyschmitz.eu>
2024-08-30 15:57:48 +0200

    feat: types unions and narrowing

--- TypeScript/dist/type.js
+++ TypeScript/dist/type.js
@@ -31,4 +31,36 @@ function Maurice() {
     var singer4 = "Aretha";
     console.log("Var: " + singer4 + ", Type: " + typeof singer4);
 }
+// Let op bij const is het van het type van de waarde, maar in javascipt nog altijd een string
+const asinger = "Hello";
+console.log("Var: " + asinger + ", Type: " + typeof asinger);
+let asinger2 = "naam";
+// fout: m een error
+asinger2 = 123;
+// fout:
+asinger2 = true;
+let asinger3 = "naam";
+// fout:
+asinger3 = 123;
+// fout:
+asinger3 = true;
+//
+// Narrowing
+//
+let itCanbe = Math.random() > 0.5 ? "maurice" : 25;
+if (typeof itCanbe === "number") {
+    // verander het type
+    let a = itCanbe / 3;
+    console.log(a);
+}
+if (typeof itCanbe === "string") {
+    let a = itCanbe.toLowerCase();
+    console.log(a);
+}
+// why ===
+let num = 10;
+let str = "10";
+console.log(num == str); // true - The values are the same after type conversion
+console.log(num === str);
+let bool = true;
 //# sourceMappingURL=type.js.map
--- TypeScript/type.ts
+++ TypeScript/type.ts
@@ -39,3 +39,45 @@ function Maurice() {
   var singer4: string = "Aretha";
   console.log("Var: " + singer4 + ", Type: " + typeof singer4);
 }
+
+// Let op bij const is het van het type van de waarde, maar in javascipt nog altijd een string
+const asinger = "Hello";
+console.log("Var: " + asinger + ", Type: " + typeof asinger);
+
+let asinger2: string = "naam";
+// fout: m een error
+asinger2 = 123;
+// fout:
+asinger2 = true;
+
+let asinger3: string | boolean = "naam";
+// fout:
+asinger3 = 123;
+// fout:
+asinger3 = true;
+
+//
+// Narrowing
+//
+let itCanbe = Math.random() > 0.5 ? "maurice" : 25;
+
+if (typeof itCanbe === "number") {
+  // verander het type
+  let a = itCanbe / 3;
+  console.log(a);
+}
+
+if (typeof itCanbe === "string") {
+  let a = itCanbe.toLowerCase();
+  console.log(a);
+}
+
+// why ===
+
+let num: number = 10;
+let str: string = "10";
+
+console.log(num == str); // true - The values are the same after type conversion
+console.log(num === str);
+
+let bool = true;
