25aad82cf288aa004c8e695b47686ae27333e46f
Maurice Schmitz <maurice@familyschmitz.eu>
2024-09-13 16:31:26 +0200

    feat: gitignore

--- TypeScript/dist/module1.js
+++
@@ -1,9 +0,0 @@
-export const name = "hello module01";
-export function SayHello(tekst) {
-    console.log(tekst);
-    return SaySomeElse("via Hello " + tekst);
-}
-function SaySomeElse(tekst) {
-    console.log(tekst);
-    return "Say Iets Anders " + tekst;
-}
--- TypeScript/dist/useModules.js
+++
@@ -1,6 +0,0 @@
-import { name, SayHello } from "./module1.js";
-// Kan niet, zelfde namen
-// import { name, SayHello } from "./module2";
-SayHello(name);
-let a = true;
-console.log(a);
