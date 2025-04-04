feb03f188790a7d11f0e4b2b5b013afb27f71c0c
OFFICE01\ed76003 <maurice.schmitz@apg.nl>
2025-04-04 14:23:01 +0200

    feat: validation show errors

--- useFormTyped/src/App.tsx
+++ useFormTyped/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm, SubmitHandler } from "react-hook-form"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type InputsData = {
+  example: string,
+  exampleRequired: string,
+  leeftijd: number
 }
 
-export default App
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<InputsData>()
+  const onSubmit: SubmitHandler<InputsData> = (data) => console.log(data)
+
+
+  console.log(watch("example")) // watch input value by passing the name of it
+
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+
+      <input defaultValue="10" {...register("leeftijd", {required: true, min: 25, max:90})}></input>
+
+      <div>
+      <h3>Errror's</h3>
+      {errors.exampleRequired?.type === "required" && <p>This field is required</p>}
+      {errors.leeftijd?.type === "required" && <p>This field is required</p>}
+      {errors.leeftijd?.type === "min" && <p>Minimaal 25 jaar</p>}
+      {errors.leeftijd?.type === "max" && <p>Maximale 90 jaar</p>}
+
+
+      {errors.leeftijd && <span>{errors.leeftijd.message}</span>}
+      </div>
+
+
+      <input type="submit" />
+    </form>
+  )
+}
