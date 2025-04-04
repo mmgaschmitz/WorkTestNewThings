182d880f858ea533802bf69544ed10fb8b409b2f
OFFICE01\ed76003 <maurice.schmitz@apg.nl>
2025-04-04 14:30:18 +0200

    feat: opmaak

--- useFormTyped/src/App.tsx
+++ useFormTyped/src/App.tsx
@@ -24,28 +24,26 @@ export default function App() {
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-
-      <input defaultValue="10" {...register("leeftijd", {required: true, min: 25, max:90})}></input>
-
-      <div>
+        <h3>Fields in a form</h3>
+    
+      <p>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+      </p>
+        <p>
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+      </p>
+      <p>
+        <input defaultValue="10" {...register("leeftijd", {required: true, min: 25, max:90})}></input>
+      </p>
       <h3>Errror's</h3>
-      {errors.exampleRequired?.type === "required" && <p>This field is required</p>}
-      {errors.leeftijd?.type === "required" && <p>This field is required</p>}
-      {errors.leeftijd?.type === "min" && <p>Minimaal 25 jaar</p>}
-      {errors.leeftijd?.type === "max" && <p>Maximale 90 jaar</p>}
-
-
-      {errors.leeftijd && <span>{errors.leeftijd.message}</span>}
-      </div>
-
-
+        {errors.exampleRequired?.type === "required" && <p>This field is required</p>}
+        {errors.leeftijd?.type === "required" && <p>This field is required</p>}
+        {errors.leeftijd?.type === "min" && <p>Minimaal 25 jaar</p>}
+        {errors.leeftijd?.type === "max" && <p>Maximale 90 jaar</p>}
+        {errors.leeftijd && <p>{errors.leeftijd.message}</p>}
       <input type="submit" />
     </form>
   )
