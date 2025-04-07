3272e3be42fd97f27ff14f2951ea3c08c5298d7e
OFFICE01\ed76003 <maurice.schmitz@apg.nl>
2025-04-07 11:22:01 +0200

    feat: mix yup en eigen melding dubbel

--- useFormTyped/src/AppWithYup.tsx
+++ useFormTyped/src/AppWithYup.tsx
@@ -1,8 +1,10 @@
-import { useForm, SubmitHandler } from "react-hook-form"
+
+import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 
 // https://strapi.io/blog/yup-validation-in-react-hook-form-a-complete-guide-with-examples
+// https://react-hook-form.com/get-started
 
 type InputsDataType = {
   example: string,
@@ -18,7 +20,7 @@ const myData: InputsDataType  = {
 
 console.log(myData);
 
-const InputsSchema = yup.object(
+const InputsSchema = yup.object().shape(
   {
     example: yup.string().trim().uppercase(),
     exampleRequired:  yup.string().required("A is verplicht").trim(),
@@ -26,10 +28,14 @@ const InputsSchema = yup.object(
   }
 );
 
+type InputsSchemaType = yup.InferType<typeof InputsSchema>;
+
+
+
 // Validate to yup schema.
 try
 {
-  InputsSchema.validate(myData);
+  InputsSchema.validateSync(myData);
 }
 catch(error)
 {
@@ -39,17 +45,20 @@ catch(error)
   console.log(message);
 }
 
-
-// const TypeInputSchema =  yup.InferType<typeof InputsSchema>;
-
 export default function App() {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<InputsDataType>()
-  const onSubmit: SubmitHandler<InputsDataType> = (data) => console.log(data)
+  } = useForm(
+    {
+       resolver: yupResolver(InputsSchema),
+    }
+  );
+
+
+  const onSubmit = (data: InputsSchemaType) => console.log(data)
 
 
   console.log(watch("example")) // watch input value by passing the name of it
@@ -66,17 +75,17 @@ export default function App() {
       </p>
         <p>
         {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
+        <input {...register("exampleRequired")} />
         {/* errors will return when field validation fails  */}
       </p>
       <p>
-        <input defaultValue="10" {...register("leeftijd", {required: true, min: 25, max:90})}></input>
+        <input defaultValue="10" {...register("leeftijd")}></input>
       </p>
       <h3>Errror's</h3>
-        {errors.exampleRequired?.type === "required" && <p>This field is required</p>}
-        {errors.leeftijd?.type === "required" && <p>This field is required</p>}
-        {errors.leeftijd?.type === "min" && <p>Minimaal 25 jaar</p>}
-        {errors.leeftijd?.type === "max" && <p>Maximale 90 jaar</p>}
+        {errors.exampleRequired?.type === "required" && <p>This field is required (DOT IT SELF)</p>}
+        {errors.leeftijd?.type === "required" && <p>This field is required (DOT IT SELF)</p>}
+        {errors.leeftijd?.type === "min" && <p>Minimaal 25 jaar (DOT IT SELF)</p>}
+        {errors.leeftijd?.type === "max" && <p>Maximale 90 jaar (DOT IT SELF)</p>}
         {errors.leeftijd && <p>{errors.leeftijd.message}</p>}
       <input type="submit" />
     </form>
