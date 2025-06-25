621718b25b6b6ec5ec9751b63ea3d03d796c2b42
OFFICE01\ed76003 <maurice.schmitz@apg.nl>
2025-06-25 21:30:39 +0200

    feat: dependency injection

---
+++ ConsoleAppDepInjection/ConsoleAppDepInjection/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppDepInjection;
+using ConsoleAppDepInjection.DepenWithServiceProvider;
+using Microsoft.Extensions.DependencyInjection;
+using Application = ConsoleAppDepInjection.DepenWithServiceProvider.Application;
+using Depend = ConsoleAppDepInjection.Depend;
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+// Geen depency injection
+Worker work1 = new();
+
+work1.DoSomething("Hello");
+work1.DoSomething("Hello2");
+
+
+// Zelf de depency vullen
+
+Depend.Worker work2 = new Depend.Worker(new Depend.MessageWriter());
+work2.DoSomething("actie3");
+
+Depend.Worker work3 = new Depend.Worker(new Depend.MessageWriterTwo());
+work3.DoSomething("actie4");
+
+
+// Dependency injection zoals bij servces
+// de service
+var services = ProgramSetup.CreateServices();
+// de logica
+Application app = services.GetRequiredService<Application>();
+app.RunIt();
+
+
+
+
